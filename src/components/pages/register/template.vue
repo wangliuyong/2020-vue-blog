6424501de00672f8ceb00c0ef175df45f6522f90
王刘永 <1355498705@qq.com>
2020-03-02 11:09:16 +0800

    add  ant-design

--- src/components/pages/register/template.vue
+++ src/components/pages/register/template.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <a-button type="primary">Primary</a-button>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import Antd from 'ant-design-vue'
 import router from './router'
+
+import 'ant-design-vue/dist/antd.css'
+
+Vue.use(Antd)
 
 Vue.config.productionTip = false
 
