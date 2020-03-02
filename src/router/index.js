7a32576320bf64eb789aadb1a2126de37ee1fab1
王刘永 <1355498705@qq.com>
2020-03-02 21:27:30 +0800

    路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -3,54 +3,96 @@ import Router from 'vue-router'
 import store from '../store/index'
 
 
-import Login from '@/components/pages/login/template.vue'
-import Register from '@/components/pages/register/template.vue'
-import Index from '@/components/pages/index/template.vue'
-import Detail from '@/components/pages/detail/template.vue'
-import Edit from '@/components/pages/edit/template.vue'
-import Create from '@/components/pages/create/template.vue'
-import User from '@/components/pages/user/template.vue'
-import My from '@/components/pages/my/template.vue'
+// import Login from '@/components/pages/login/template.vue'
+// import Register from '@/components/pages/register/template.vue'
+// import Index from '@/components/pages/index/template.vue'
+// import Detail from '@/components/pages/detail/template.vue'
+// import Edit from '@/components/pages/edit/template.vue'
+// import Create from '@/components/pages/create/template.vue'
+// import User from '@/components/pages/user/template.vue'
+// import My from '@/components/pages/my/template.vue'
 
 Vue.use(Router)
 
-const router = new Router({
+// const router = new Router({
+//   routes: [
+//     {
+//       path: '/',
+//       component: Index
+//     },
+//     {
+//       path: '/login',
+//       component: Login
+//     },
+//     {
+//       path: '/register',
+//       component: Register
+//     },
+//     {
+//       path: '/detail/:blogId',
+//       component: Detail
+//     },
+//     {
+//       path: '/edit/:blogId',
+//       component: Edit,
+//       meta:{requiresAuth:true}
+//     },
+//     {
+//       path: '/create',
+//       component: Create,
+//       meta:{requiresAuth:true}
+//     },
+//     {
+//       path: '/user/:userId',
+//       component: User
+//     },
+//     {
+//       path: '/my',
+//       component: My,
+//       meta:{requiresAuth:true}
+//     },
+//   ]
+// })
+
+//打包，异步加载对应的模块，避免了同时加载所有模块导致打包后文件过大
+const router= new Router({
   routes: [
     {
       path: '/',
-      component: Index
+      component: ()=>{return import('@/components/pages/index/template.vue')}
     },
     {
       path: '/login',
-      component: Login
+      component: ()=>{return import('@/components/pages/login/template.vue')}
     },
     {
       path: '/register',
-      component: Register
+      component: ()=>{return import('@/components/pages/register/template.vue')}
     },
     {
       path: '/detail/:blogId',
-      component: Detail
+      component: ()=>{return import('@/components/pages/detail/template.vue')}
     },
     {
       path: '/edit/:blogId',
-      component: Edit,
+      component: ()=>{return import('@/components/pages/edit/template.vue')},
       meta:{requiresAuth:true}
     },
     {
       path: '/create',
-      component: Create,
+      component: ()=>{return import('@/components/pages/create/template.vue')},
       meta:{requiresAuth:true}
     },
     {
       path: '/user/:userId',
-      component: User
+      component: ()=>{return import('@/components/pages/user/template.vue')}
     },
     {
       path: '/my',
-      component: My,
+      component: ()=>{return import('@/components/pages/my/template.vue')},
       meta:{requiresAuth:true}
     },
+    
   ]
 })
 
