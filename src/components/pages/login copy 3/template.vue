7cbd691c15f00c59eeac0f63f20e870dde49aa6f
王刘永 <1355498705@qq.com>
2020-03-02 10:47:14 +0800

    创建路由及其对应的页面

---
+++ src/components/pages/login copy 3/template.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script src="./template.js">
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less" scoped src="./template.less">
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/pages/HelloWorld'
 import Login from '@/components/pages/login/template.vue'
+import Register from '@/components/pages/register/template.vue'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/login',
       name: 'Login',
       component: Login
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register
     }
   ]
 })
